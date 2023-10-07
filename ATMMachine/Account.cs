a210a815e6daf2d0d9a4c1558fefb05556bc0b79
cmesq <cmesq@SHIRATOR>
2023-10-07 18:05:28 -0400

    Add project files.

---
+++ ATMMachine/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class Account
+    {
+    string name;
+    int accountId;
+    double balance;
+
+    public Account(string name, int accountId, double balance)
+    {
+        this.name = name;
+        this.accountId = accountId; 
+        this.balance = balance;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (balance >= amount)
+        {
+            balance -= amount;
+            Console.WriteLine($"\tYour new balance is {balance}\n");
+        }
+        else
+        {
+            Console.WriteLine("\tYou don't have enough balance! =(\n");
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        Console.WriteLine($"\tYour new balance is {balance}\n");
+    }
+
+    public double CheckBalance()
+    {
+        return balance;
+    }
+}
---
+++ ATMMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account myAccount = new("Carlos Mesquita", 2525, 850.25);
+
+        Console.WriteLine("Your current balance is: " + myAccount.CheckBalance());
+
+        Console.WriteLine("Depositing 500$...\n");
+        myAccount.Deposit(500);
+        Console.WriteLine("Taking 200$ for the bills...\n");
+        myAccount.Withdraw(200);
+        Console.WriteLine("Taking 1000$ to buy a PS5!!!\n");
+        myAccount.Withdraw(1000);
+    }
+}
